4c8bfa10761da6137d367326fc6b934196dbe3cf
Bradley Duck <bradley@appoly.co.uk>
2025-05-22 12:14:18 +0100

    Cleaning up - Removed unused method

--- reorderable/src/commonMain/kotlin/org/burnoutcrew/reorderable/DragGesture.kt
+++ reorderable/src/commonMain/kotlin/org/burnoutcrew/reorderable/DragGesture.kt
@@ -88,60 +88,6 @@ internal suspend fun AwaitPointerEventScope.awaitPointerSlopOrCancellation(
     }
 }
 
-internal suspend fun PointerInputScope.awaitLongPressOrCancellation(
-    initialDown: PointerInputChange
-): PointerInputChange? {
-    var longPress: PointerInputChange? = null
-    var currentDown = initialDown
-    val longPressTimeout = viewConfiguration.longPressTimeoutMillis
-    return try {
-        // wait for first tap up or long press
-        withTimeout(longPressTimeout) {
-            awaitPointerEventScope {
-                var finished = false
-                while (!finished) {
-                    val event = awaitPointerEvent(PointerEventPass.Main)
-                    if (event.changes.fastAll { it.changedToUpIgnoreConsumed() }) {
-                        // All pointers are up
-                        finished = true
-                    }
-
-                    if (
-                        event.changes.fastAny {
-                            it.isConsumed || it.isOutOfBounds(size, extendedTouchPadding)
-                        }
-                    ) {
-                        finished = true // Canceled
-                    }
-
-                    // Check for cancel by position consumption. We can look on the Final pass of
-                    // the existing pointer event because it comes after the Main pass we checked
-                    // above.
-                    val consumeCheck = awaitPointerEvent(PointerEventPass.Final)
-                    if (consumeCheck.changes.fastAny { it.isConsumed }) {
-                        finished = true
-                    }
-                    if (!event.isPointerUp(currentDown.id)) {
-                        longPress = event.changes.fastFirstOrNull { it.id == currentDown.id }
-                    } else {
-                        val newPressed = event.changes.fastFirstOrNull { it.pressed }
-                        if (newPressed != null) {
-                            currentDown = newPressed
-                            longPress = currentDown
-                        } else {
-                            // should technically never happen as we checked it above
-                            finished = true
-                        }
-                    }
-                }
-            }
-        }
-        null
-    } catch (_: TimeoutCancellationException) {
-        longPress ?: initialDown
-    }
-}
-
 private fun PointerEvent.isPointerUp(pointerId: PointerId): Boolean =
     changes.fastFirstOrNull { it.id == pointerId }?.pressed != true
 
